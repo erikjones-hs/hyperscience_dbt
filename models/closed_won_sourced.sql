63d94244253c963ed21c00727c275fc6c63250a4
Erik Jones <erik.jones@hyperscience.com>
2022-03-30 17:51:38 +0000

    added in opp_lead_scource dimension

--- models/closed_won_sourced.sql
+++ models/closed_won_sourced.sql
@@ -572,11 +572,12 @@ fy_agg_int as (
 select distinct
 fy_year, 
 dte,
+opp_lead_source,
 sum(opp_net_new_arr) as net_new_arr,
 sum(opp_arr) as total_arr,
 count(distinct opp_id) as num_opps
 from fct_sourced
-group by dte, fy_year
+group by opp_lead_source, dte, fy_year
 order by dte asc
 ),
 
@@ -584,12 +585,13 @@ fy_agg as (
 select 
 fy_year,
 to_timestamp(dte) as dte,
-ZEROIFNULL(sum(net_new_arr) over (partition by fy_year order by dte asc rows between unbounded preceding and current row)) as net_new_arr,
-ZEROIFNULL(sum(total_arr) over (partition by fy_year order by dte asc rows between unbounded preceding and current row)) as total_arr,
-ZEROIFNULL(sum(num_opps) over (partition by fy_year order by dte asc rows between unbounded preceding and current row)) as num_opps
+opp_lead_source,
+ZEROIFNULL(sum(net_new_arr) over (partition by fy_year, opp_lead_source order by dte asc rows between unbounded preceding and current row)) as net_new_arr,
+ZEROIFNULL(sum(total_arr) over (partition by fy_year, opp_lead_source order by dte asc rows between unbounded preceding and current row)) as total_arr,
+ZEROIFNULL(sum(num_opps) over (partition by fy_year, opp_lead_source order by dte asc rows between unbounded preceding and current row)) as num_opps
 from fy_agg_int 
 where to_date(dte) <= to_date(current_date())
-order by fy_year asc, dte asc
+order by fy_year asc, dte asc, opp_lead_source
 )
 
 select * from fy_agg
