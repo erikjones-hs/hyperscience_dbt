6b3f798e04ad2128d5197d860e341fb7548dde56
Erik Jones <erik.jones@hyperscience.com>
2022-04-06 21:19:36 +0000

    updated fct_arr_opp to reference arr_opp_history model

--- models/fct_arr_opp.sql
+++ models/fct_arr_opp.sql
@@ -6,5 +6,501 @@
 )
 }}
 
---select * from dev.erikjones.monthly_kpis_finance_arr 
-select * from dev.erikjones.monthly_kpis_finance_arr_trash
+/* Pulling in raw SFDC data from arr_opp_history model*/
+with raw_data as (
+select * from {{ref('arr_opp_history')}}
+),
+
+/* transforming start dates to start months */
+/* adjusting start and end dates to match historical finance data from ARR Google Sheet */
+/* adjusting arr to match historical finance data from ARR Google Sheet */
+raw_data_transformed as (
+select 
+CASE WHEN account_id = '0011R000026iP6rQAE' then '0013600001iRke2AAC' else account_id end as account_id,
+CASE WHEN account_name = 'TD Ameritrade' then 'Charles Schwab' else account_name end as account_name,
+opp_id,
+opp_name,
+opp_revenue_type,
+CASE WHEN opp_id = '0063600000X36zWAAR' then to_date('2020-07-01')
+     WHEN opp_id = '0061R00000m11f1QAA' then to_date('2020-07-01')
+     WHEN opp_id = '0061R00000tE0SIQA0' then to_date('2021-06-01')
+     WHEN opp_id = '0063600000eMMZeAAO' then to_date('2019-12-01')
+     WHEN opp_id = '0061R00000pmValQAE' then to_date('2020-06-17')
+     WHEN opp_id = '0063600000FJwn2AAD' then to_date('2020-07-31')
+     WHEN opp_id = '0063600000iS5wbAAC' then to_date('2020-01-01')
+     WHEN opp_id = '0061R00000r6r1iQAA' then to_date('2023-11-15') /* Adjusting the end date for CRL because they are still a customer, but with no committed revenue */
+     WHEN OPP_ID = '0061R00000r83epQAA' THEN TO_DATE('2020-09-15')
+     WHEN opp_id = '0061R00000wKxG5QAK' THEN to_date('2020-08-15')
+     WHEN opp_id = '0061R00000uJr07QAC' THEN to_date('2021-08-15')
+     WHEN opp_id = '0061R000010tCbNQAU' THEN to_date('2020-10-15')
+     WHEN opp_id = '0061R000010tCZvQAM' THEN to_date('2021-10-15')
+     WHEN opp_id = '0061R00000oERITQA4' THEN to_date('2022-10-15')
+     WHEN opp_id = '0063600000M73LuAAJ' then to_date('2020-02-15')
+     WHEN opp_id = '0063600000dsPyXAAU' then to_date('2019-10-15')
+     WHEN opp_id = '0061R00000kRNPDQA4' then to_date('2020-10-15')
+     WHEN opp_id = '0063600000dsPsjAAE' then to_date('2020-08-15')
+     WHEN opp_id = '0061R00000zAM8wQAG' then to_date('2021-08-15')
+     WHEN opp_id = '0061R00000tFLB3QAO' then to_date('2021-11-15')
+     WHEN opp_id = '0061R0000137jsqQAA' then to_date('2022-08-30')
+     WHEN opp_id = '0061R0000137jqkQAA' then to_date('2022-08-19')
+     WHEN opp_id = '0061R0000137ijiQAA' then to_date('2022-06-09')
+     WHEN opp_id = '0061R00000uL8ylQAC' then to_date('2023-11-15') /* Adjusting the end date for PMP because they are still a customer, but with a 1 year free contract period */
+     WHEN opp_id = '0061R00000zAjoeQAC' then to_date('2021-10-15')
+     WHEN opp_id = '0061R0000137hOKQAY' then to_date('2022-08-20')
+     WHEN opp_id = '0061R000010PVABQA4' then to_date('2021-10-15')
+     WHEN opp_id = '0061R00000zBqNRQA0' then to_date('2021-11-15')
+     WHEN opp_id = '0061R000013fGLrQAM' then to_date('2022-12-18')
+     WHEN opp_id = '0061R000013fGTbQAM' then to_date('2022-11-23') /* Updated end date because it is incorect in SFDC. divvyDose 180k */
+     WHEN opp_id = '0061R00000zD2sTQAS' then to_date('2021-12-15') /* End date adjusted per Kristen and Finance ARR Google Sheet. Conduent 280k */ 
+     WHEN opp_id = '0061R000010t71kQAA' then to_date('2022-01-15') /* Customer no longer is paying. Close this out in Jan. per FP&A. Sience SAS 41.65k */
+     when opp_id = '0061R000014uXZrQAM' then to_date('2023-01-25') /* Updated MPOWER end date because it is incorrect in SFDC */
+     when opp_id = '0061R00000yElHXQA0' then to_date('2022-02-15') /* Customer Churned in Feb, per FP&A. Department of Treasury 87.5k */
+     when opp_id = '0061R000010O65hQAC' then to_date('2022-08-15') /* End date adjustment because of open negotiations. First American Financial 1M */
+     when opp_id = '0061R00000tG3b2QAC' then to_date('2022-05-15') /* End date adjustment because of open negotiations. CI Financial 150k */
+     when opp_id = '0061R00000zAlU8QAK' then to_date('2022-03-15') /* Opportunity churned in March, per Kristen. AMEX 323k */
+     when opp_id = '0061R0000136hnzQAA' then to_date('2022-02-15') /* Customer churned in Feb. per Kristen. AXA Churn. 35k */ 
+     when opp_id = '0061R000014vAD7QAM' then to_date('2023-02-15') /* Adjusting end date because it is incorrect in SFDC */
+     when opp_id = '0061R00000r7xPhQAI' then to_date('2022-02-15') /* Customer churned. Close this out in Feb. per FP&A. DISA 64.3k */ 
+     when opp_Id = '0061R0000137tYlQAI' then to_date('2022-03-15') /* Customer churned. Close this out in Mar. per FP&A. Record Connect 239k */
+     when opp_id = '0061R0000137kNxQAI' then to_date('2022-04-15') /* Customer Churned in April per FP&A. State of Texas 402.5k total. 17.5k opp */
+     when opp_id = '0061R00000zD2sxQAC' then to_date('2022-05-15') /* End date adjustment because of open negotiations. Conduent 1.98M */
+     ELSE end_dte_raw end as end_dte,
+CASE WHEN opp_id = '0061R00000uINyXQAW' then to_date('2020-08-01')
+     WHEN opp_id = '0061R00000uIehuQAC' then to_date('2020-01-01')
+     WHEN opp_id = '0061R00000zD2sxQAC' then to_date('2020-12-01')
+     WHEN opp_id = '0063600000FJwn2AAD' then to_date('2019-07-01')
+     WHEN opp_id = '0061R00000pmValQAE' then to_date('2019-06-15')
+     WHEN opp_id = '0061R00000tF1MSQA0' then to_date('2020-10-15')
+     WHEN opp_id = '0061R00000zAM8wQAG' then to_date('2020-09-15')
+     WHEN opp_id = '0061R00000zDAxAQAW' then to_date('2020-02-01')
+     WHEN opp_id = '0063600000X36zWAAR' then to_date('2018-04-01')
+     WHEN opp_id = '0063600000kQAyCAAW' then to_date('2018-12-15')
+     WHEN opp_id = '0061R00000pkoODQAY' then to_date('2019-08-15')
+     WHEN opp_id = '0063600000eMMZeAAO' then to_date('2018-12-15')
+     WHEN opp_id = '0063600000dsPsjAAE' then to_date('2018-06-15')
+     WHEN opp_id = '0063600000M73LuAAJ' then to_date('2019-02-15')
+     WHEN opp_id = '0063600000dsPyXAAU' then to_date('2018-10-15')
+     WHEN opp_id = '0061R00000kRNPDQA4' then to_date('2019-10-15')
+     WHEN opp_id = '0061R0000137ijiQAA' then to_date('2020-06-30')
+     WHEN opp_id = '0061R000013flkIQAQ' then to_date('2021-10-15')
+     WHEN opp_id = '0061R000014xeQwQAI' then to_date('2022-01-15')
+     WHEN opp_id = '0061R0000135gO1QAI' then to_date('2021-12-15')
+     WHEN opp_id = '0061R0000137hXuQAI' then to_date('2022-02-15')
+     WHEN opp_id = '0061R0000136ZbBQAU' then to_date('2022-03-15')
+     ELSE start_dte_raw end as start_dte,
+date_trunc('month',to_date(start_dte)) as start_dte_month,
+date_trunc('month',to_date(end_dte)) as end_dte_month,
+CASE WHEN opp_id = '0063600000M73LuAAJ' then 200000
+     WHEN opp_id = '0063600000dsPyXAAU' then 150000
+     WHEN opp_id = '0061R00000kRNPDQA4' then 400000
+     WHEN opp_id = '0063600000X3OBrAAN' then 480000
+     WHEN opp_id = '0061R0000137ijiQAA' then 25000
+     WHEN opp_id = '0063600000dsPsjAAE' then 600000
+     WHEN opp_id = '0061R00000uJr07QAC' then 100000
+     WHEN opp_id = '0061R0000135gO1QAI' then 89040
+     when opp_id = '0061R000014xeQwQAI' then 13269
+     ELSE opp_arr end as opp_arr,
+CASE WHEN opp_id = '0061R0000135gO1QAI' then 5040 
+     WHEN opp_id = '0061R000014xeQwQAI' then 13269
+     ELSE opp_net_new_arr end as opp_net_new_arr
+from raw_data
+where opp_id not in ('00636000003gG2qAAE','0063600000W0NhNAAV','0063600000SKDdAAAX','0061R00000m1g4KQAQ','0063600000X36vUAAR') /*removing these ops per FP&A */
+),
+
+/* Subscription period intermediate view */
+/* each row is an opportunity with transformed start and end dates */
+/* one last adjustment to end months to account for opportunities that start on the 1st of a month and end on the last day of the month */
+subscription_periods as (
+select 
+account_id,
+account_name,
+opp_id,
+opp_name,
+opp_revenue_type,
+start_dte_month,  
+start_dte,
+end_dte,
+CASE WHEN (end_dte = last_day(end_dte_month) and start_dte = start_dte_month) then dateadd(month,1,end_dte_month)  
+     else end_dte_month end as end_dte_month,
+opp_arr,
+opp_net_new_arr
+from raw_data_transformed
+),
+
+/* dim dates table */
+months as (
+select distinct
+date_trunc('month',dte) as date_month
+from "DEV"."MARTS"."FY_CALENDAR"
+),
+
+/* each row in this table is an oppportunity - month combination */
+/* making some further historical adjustments to ARR per FP&A */
+opportunity_months as (
+select 
+months.date_month,
+sp.account_id,
+sp.account_name,
+sp.opp_id,
+sp.opp_name,
+sp.opp_revenue_type,
+sp.start_dte_month,
+sp.start_dte,
+sp.end_dte_month,
+sp.end_dte,
+sp.opp_arr,
+sp.opp_net_new_arr
+from subscription_periods as sp
+inner join months on (months.date_month >= sp.start_dte_month AND months.date_month < sp.end_dte_month)
+),
+
+/* fanning out opportunities by month */
+/* each row is an opporunity - month combination with zeros filled in for missing months */
+/* each opportunity should have a record every month, for months between it's start month and end month */ 
+joined_opportunity_int as (
+select 
+om.date_month,
+om.account_id,
+om.account_name,
+om.opp_id,
+om.opp_name,
+om.opp_revenue_type,
+om.start_dte_month,
+om.start_dte,
+om.end_dte_month,
+om.end_dte,
+om.opp_arr,
+om.opp_net_new_arr,
+coalesce(subscription_periods.opp_arr,0) as mrr
+from opportunity_months as om
+left join subscription_periods on (om.opp_id = subscription_periods.opp_id AND om.date_month >= subscription_periods.start_dte_month AND om.date_month < subscription_periods.end_dte_month)
+),
+
+/* making final adjustmnents to historical mrr per FP&A */
+joined_opportunity as (
+select
+joi.date_month,
+joi.account_id,
+joi.account_name,
+joi.opp_id,
+joi.opp_name,
+joi.opp_revenue_type,
+joi.start_dte_month,
+joi.start_dte,
+joi.end_dte_month,
+joi.end_dte,
+joi.opp_arr,
+joi.opp_net_new_arr,
+CASE WHEN opp_id = '0063600000X36zWAAR' and to_date(joi.date_month) <= '2019-06-01' then 128000 
+     WHEN opp_id = '0063600000dsPsjAAE' and to_date(joi.date_month) <= '2019-07-01' then 560000 
+     when opp_id = '0061R00000r6r1iQAA' and to_date(joi.date_month) >= '2021-11-01' then 0.00000000001
+     when opp_id = '0061R00000uL8ylQAC' and to_date(joi.date_month) >= '2021-11-01' then 0.00000000001
+     ELSE joi.mrr end as mrr
+from joined_opportunity_int as joi 
+),
+
+/* adding in binary features for active, first active month, last active month, first month and last month */
+final_opp as (
+select 
+date_month,
+account_id,
+account_name,
+opp_id,
+opp_name,
+opp_revenue_type,
+start_dte_month,
+start_dte,
+end_dte_month,
+end_dte,
+opp_arr,
+opp_net_new_arr,
+mrr,
+mrr > 0 as is_active, 
+min(case when is_active then date_month end) over (partition by opp_id) as first_active_month,
+max(case when is_active then date_month end) over (partition by opp_id) as last_active_month,
+first_active_month = date_month as is_first_month,
+last_active_month = date_month as is_last_month
+from joined_opportunity as jo
+),
+
+/* identifying churned month for each opportunity */
+churn_opp as (
+select 
+dateadd(month,1,date_month)::date as date_month,
+account_id,
+account_name,
+opp_id,
+opp_name,
+opp_revenue_type,
+start_dte_month,
+start_dte,
+end_dte_month,
+end_dte,
+0::float as opp_arr,
+0::float as opp_net_new_arr,
+0::float as mrr,
+false as is_active,
+first_active_month,
+last_active_month,
+false as is_first_month,
+false as is_last_month
+from final_opp
+where is_last_month
+),
+
+/* combining non-churned months with churned month for opportunities */
+/* last entry for each opp id is the churn month, zero'ing out mrr */
+/* now we have a full historical transaction record for each opp id */
+/* first record is the start month, last record is the churn month with zero'ed out mrr */
+unioned_opp as (
+select * from final_opp 
+UNION ALL 
+select * from churn_opp
+),
+
+/* for any opportunity - month, looking back one month and forward one month to be able to classify mrr changes correctly */
+mrr_with_changes_opp as (
+select 
+date_month,
+account_id,
+account_name,
+opp_id,
+opp_name,
+opp_revenue_type,
+start_dte_month,
+start_dte,
+end_dte_month,
+end_dte,
+opp_arr,
+opp_net_new_arr,
+mrr,
+is_active, 
+first_active_month,
+last_active_month,
+is_first_month,
+is_last_month,
+coalesce(lag(is_active) over (partition by opp_id order by date_month),false) as previous_month_is_active,
+coalesce(lead(is_active) over (partition by opp_id order by date_month),false) as next_month_is_active,
+coalesce(lag(mrr) over (partition by opp_id order by date_month),0) as previous_month_mrr,
+coalesce(lead(mrr) over (partition by opp_id order by date_month),0) as next_month_mrr,
+mrr - previous_month_mrr as mrr_change
+from unioned_opp
+),
+
+/* ######################################################################## */
+/* ######################################################################## */
+/* NOW DOING THE SAME THING FOR ACCOUNTS THAT WE JUST DID FOR OPPORTUNITIES */
+
+/* summing mrr by account and month */
+/* identifying an active account */
+account_int as (
+select distinct 
+date_month,
+account_id,
+CASE WHEN sum(mrr) over (partition by account_id, date_month) > 0 THEN true else false end as is_active_acct,
+sum(mrr) over (partition by account_id, date_month) as account_mrr 
+from final_opp
+),
+
+/* idnetifying first and last active months for an account */
+account as (
+select distinct 
+date_month,
+account_id,
+is_active_acct,
+account_mrr,
+min(case when is_active_acct then date_month end) over (partition by account_id) as first_active_month_acct,
+max(case when is_active_acct then date_month end) over (partition by account_id) as last_active_month_acct
+from account_int
+),
+
+/* adding binary identifiers for is_first_month and is_last_month for accounts */
+final_acct as (
+select distinct
+date_month,
+account_id,
+is_active_acct,
+account_mrr,
+first_active_month_acct,
+last_active_month_acct,
+first_active_month_acct = date_month as is_first_month_acct,
+last_active_month_acct = date_month as is_last_month_acct
+from account 
+),
+
+/* identifying churned month for each opportunity */
+churn_acct as (
+select 
+dateadd(month,1,date_month)::date as date_month,
+account_id,
+false as is_active_acct,
+0::float as account_mrr,
+first_active_month_acct,
+last_active_month_acct,
+false as is_first_month_acct,
+false as is_last_month_acct
+from final_acct
+where is_last_month_acct
+),
+
+/* combining non-churned months with churned month for accounts */
+/* last entry for each account id is the churn month, zero'ing out mrr */
+/* now we have a full historical transaction record for each account id */
+/* first record is the start month, last record is the churn month with zero'ed out mrr */
+unioned_acct as (
+select * from final_acct
+UNION ALL 
+select * from churn_acct
+),
+
+/* for any opportunity - month, looking back one month and forward one month to be able to classify mrr changes correctly */
+mrr_with_changes_acct as (
+select 
+date_month,
+account_id,
+is_active_acct,
+account_mrr,
+first_active_month_acct,
+last_active_month_acct,
+is_first_month_acct,
+last_active_month_acct = date_month as is_last_month_acct,
+coalesce(lag(is_active_acct) over (partition by account_id order by date_month),false) as previous_month_is_active_acct,
+coalesce(lead(is_active_acct) over (partition by account_id order by date_month),false) as next_month_is_active_acct,
+coalesce(lag(account_mrr) over (partition by account_id order by date_month),0) as previous_month_mrr_acct,
+coalesce(lead(account_mrr) over (partition by account_id order by date_month),0) as next_month_mrr_acct,
+account_mrr - previous_month_mrr_acct as mrr_change_acct
+from unioned_acct
+),
+
+/* joining opportunity and account data */
+change_table_int as (
+select
+mwco.date_month,
+mwco.account_id,
+mwco.account_name,
+mwco.opp_id,
+mwco.opp_name,
+mwco.opp_revenue_type,
+mwco.start_dte_month,
+mwco.start_dte,
+mwco.end_dte_month,
+mwco.end_dte,
+mwco.opp_arr,
+mwco.opp_net_new_arr,
+mwco.mrr,
+mwco.is_active,
+mwco.first_active_month,
+mwco.last_active_month,
+mwco.is_first_month,
+mwco.is_last_month,
+mwco.previous_month_is_active,
+mwco.next_month_is_active,
+mwco.previous_month_mrr,
+mwco.next_month_mrr,
+mwco.mrr_change,
+mwca.is_active_acct,
+mwca.first_active_month_acct,
+mwca.last_active_month_acct,
+mwca.is_first_month_acct,
+mwca.is_last_month_acct,
+mwca.account_mrr,
+mwca.previous_month_is_active_acct,
+mwca.next_month_is_active_acct,
+mwca.previous_month_mrr_acct,
+mwca.next_month_mrr_acct,
+mwca.mrr_change_acct
+from mrr_with_changes_opp as mwco
+left join mrr_with_changes_acct as mwca on (mwco.account_id = mwca.account_id AND mwco.date_month = mwca.date_month) 
+),
+
+/* categorizing opportunity, revenue and account changes */
+change_table as (
+select distinct 
+date_month,
+account_id,
+account_name,
+opp_id,
+opp_name,
+opp_revenue_type,
+start_dte_month,
+start_dte,
+end_dte_month,
+end_dte,
+opp_arr,
+opp_net_new_arr,
+mrr,
+is_active,
+first_active_month,
+last_active_month,
+is_first_month,
+is_last_month,
+previous_month_is_active,
+next_month_is_active,
+previous_month_mrr,
+next_month_mrr,
+mrr_change,
+is_active_acct,
+first_active_month_acct,
+last_active_month_acct,
+is_first_month_acct,
+is_last_month_acct,
+account_mrr,
+previous_month_is_active_acct,
+next_month_is_active_acct,
+previous_month_mrr_acct,
+next_month_mrr_acct,
+mrr_change_acct,
+CASE WHEN is_first_month = true and mrr > 0 then 'new'
+     WHEN is_active = false and previous_month_is_active = true then 'churn'
+     ELSE 'recurring'
+     END AS opp_category,
+CASE WHEN is_first_month_acct = true then 'new'
+     WHEN is_active_acct = false and previous_month_is_active_acct = true and next_month_is_active_acct = false then 'churn'
+     WHEN (is_active_acct = true OR (is_active_acct = false AND previous_month_is_active_acct = true AND next_month_is_active_acct = true)) then 'active'
+     ELSE NULL
+     END AS customer_category,
+CASE WHEN customer_category = 'new' and mrr_change_acct > 0 then 'new'
+     WHEN customer_category = 'active' and mrr_change_acct = 0 then 'recurring'
+     WHEN customer_category = 'active' and mrr_change_acct > 0 then 'expansion'
+     WHEN mrr_change_acct < 0 then 'churn'
+     ELSE NULL end as revenue_category
+from change_table_int
+),
+
+/* removing unnecessary columns from the final table */
+/* renaming so column names are consistent */
+fct_arr as (
+select distinct
+to_timestamp(date_month) as date_month,
+account_id,
+account_name,
+opp_id,
+opp_name,
+to_timestamp(start_dte_month) as start_dte_month,
+to_timestamp(start_dte) as start_dte,
+to_timestamp(end_dte_month) as end_dte_month,
+to_timestamp(end_dte) as end_dte,
+mrr,
+mrr_change,
+CASE WHEN mrr = 0 then mrr_change else mrr end as mrr_reporting,
+is_active,
+first_active_month,
+last_active_month,
+is_first_month,
+is_last_month,
+account_mrr as mrr_acct,
+mrr_change_acct,
+CASE WHEN mrr_acct = 0 then mrr_change_acct else mrr_acct end as mrr_reporting_acct,
+is_active_acct,
+first_active_month_acct,
+last_active_month_acct,
+is_first_month_acct,
+is_last_month_acct,
+opp_category,
+customer_category,
+revenue_category
+from change_table
+order by account_id, start_dte_month asc, date_month asc
+)
+
+select * from fct_arr
