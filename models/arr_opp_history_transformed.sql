5c689e103121036662474ccd46c7f1931f23901d
Erik Jones <erik.jones@hyperscience.com>
2022-06-01 21:27:03 +0000

    updated arr models for June

--- models/arr_opp_history_transformed.sql
+++ models/arr_opp_history_transformed.sql
@@ -67,8 +67,11 @@ CASE WHEN opp_id = '0063600000X36zWAAR' then to_date('2020-07-01')
      when opp_id = '0061R0000137kNxQAI' then to_date('2022-04-15') /* Customer Churned in April per FP&A. State of Texas 402.5k total. 17.5k opp */
      when opp_id = '0061R0000137hQzQAI' then to_date('2022-10-15') /* Adjusting end date by 1 month per FP&A. Allstate 15k */
      when opp_id = '0061R000010O65hQAC' then to_date('2022-08-15') /* End date adjustment because of open negotiations. First American Financial 1M */
-     when opp_id = '0061R00000zD2sxQAC' then to_date('2022-06-15') /* End date adjustment because of open negotiations. Conduent 1.98M */
-     when opp_id = '0061R00000zAIBfQAO' then to_date('2022-06-15') /* End date adjustment because of open negotiations. Manulife 375k */ 
+     when opp_id = '0061R00000zD2sxQAC' then to_date('2022-07-15') /* End date adjustment because of open negotiations. Conduent 1.98M */
+     when opp_id = '0061R00000zAIBfQAO' then to_date('2022-07-15') /* End date adjustment because of open negotiations. Manulife 375k */ 
+     when opp_id = '0061R000010PjW2QAK' then to_date('2022-07-15') /* End date adjustment because of open negotiations. Conseil 1.5k */
+     when opp_id = '0061R0000137ijiQAA' then to_date('2022-07-15') /* End date adjustment because of open negotiations. Johnson Law Group 25k */
+     when opp_id = '0061R00000uKfFzQAK' then to_date('2022-07-15') /* End date adjustment because of open negotiations. weFox 131.5k */ 
      ELSE end_dte_raw end as end_dte,
 end_dte_raw,
 CASE WHEN opp_id = '0061R00000uINyXQAW' then to_date('2020-08-01')
