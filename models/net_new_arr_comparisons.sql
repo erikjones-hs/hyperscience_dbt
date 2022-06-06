5a678b269cbd8c2d5f30f0c5cd321ec88686d963
Erik Jones <erik.jones@hyperscience.com>
2022-06-06 18:32:43 +0000

    added in new dates for net new arr comparisons

--- models/net_new_arr_comparisons.sql
+++ models/net_new_arr_comparisons.sql
@@ -18,6 +18,35 @@ group by date_month, category
 order by date_month asc
 ),
 
+fy_dates as (
+select distinct
+date_trunc('month',dte) as date_month,
+0 as arr_future
+from "DEV"."MARTS"."FY_CALENDAR"
+where date_trunc('month',dte) >= '2022-03-01'
+and date_trunc('month',dte) <= '2023-02-01' 
+),
+
+actuals_combined_int as (
+select distinct
+fd.date_month,
+CASE WHEN nnam.category IS NULL then 'actuals' else nnam.category end as category,
+nnam.arr,
+fd.arr_future
+from fy_dates as fd
+left join net_new_actuals_monthly as nnam on (to_date(nnam.date_month) = to_date(fd.date_month))
+order by date_month asc
+),
+
+actuals_combined as (
+select 
+to_timestamp(date_month) as date_month,
+category,
+CASE WHEN arr is NULL then arr_future ELSE arr end as arr
+from actuals_combined_int
+order by date_month asc
+),
+
 net_new_budget_monthly as (
 select 
 to_date(date) as date_month,
@@ -39,7 +68,7 @@ order by date_month asc
 ),
 
 fct_net_new as (
-select * from net_new_actuals_monthly
+select * from actuals_combined
 UNION 
 select * from net_new_budget_monthly
 UNION 
