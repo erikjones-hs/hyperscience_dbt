b3b776aff17cd75f0e68bc8afd678d65b320dc03
Erik Jones <erik.jones@hyperscience.com>
2022-08-31 22:14:49 +0000

    updated incremental ARR model

--- models/arr_opp_history_transformed.sql
+++ models/arr_opp_history_transformed.sql
@@ -19,6 +19,7 @@ select
 CASE WHEN account_id = '0011R000026iP6rQAE' then '0013600001iRke2AAC' else account_id end as account_id,
 CASE WHEN account_name = 'TD Ameritrade' then 'Charles Schwab' 
      WHEN account_name = '8053580156557' then 'Department of Justice' 
+     WHEN account_name = '8780895197581' then 'Mathematica, Inc.'
      else account_name end as account_name,
 opp_id,
 opp_name,
@@ -70,7 +71,6 @@ CASE WHEN opp_id = '0063600000X36zWAAR' then to_date('2020-07-01')
      when opp_id = '0061R0000137ijiQAA' then to_date('2022-06-29') /* End date adjustment due to negotiated end of contract. Johnson Law Group 25k */
      when opp_id = '0061R0000137jqkQAA' then to_date('2022-08-19') /* Adjusting end date because wrong in Salesforce. QAI 35k */
      when opp_id = '0061R00000zDCt9QAG' then to_date('2024-08-24') /* End date adjustment because renewal date was wrong in snapshot */
-     when opp_id = '0061R000010PjW2QAK' then to_date('2022-09-15') /* End date adjustment because of open negotiations. Conseil 1.5k */ 
      when opp_id = '0061R0000137jsqQAA' then to_date('2022-09-15') /* End date adjustment because of open negotiations. Pac Life 330k */
      when opp_id = '0061R000010O65hQAC' then to_date('2022-09-15') /* End date adjustment because of open negotiations. First American Financial 1M */
      when opp_id = '0061R0000137hOKQAY' then to_date('2022-09-15') /* End date adjustment because of open negotiations. SSA DeDupe 1.9M */
