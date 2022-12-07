9557d09b56fdc85ac7996f504c8b10a3bf52b6c7
Erik Jones <erik.jones@hyperscience.com>
2022-12-07 16:22:52 +0000

    updated comments

--- models/arr_prod/arr_opp_history_transformed.sql
+++ models/arr_prod/arr_opp_history_transformed.sql
@@ -93,8 +93,8 @@ CASE WHEN opp_id = '0063600000X36zWAAR' then to_date('2020-07-01')
      when opp_id = '0061R00001A4rKQQAZ' then to_date('2023-11-30') /* End date adjustment because it is wrong in SFDC. Kovack 35k */
      when opp_id = '0061R00000zBWKMQA4' then to_date('2022-11-15') /* End date adjustment due to upsell. Momentum 330.67k */
      when opp_id = '0061R00001A4rFVQAZ' then to_date('2023-12-18') /* End date adjustment because it is wrong in SFDC. Legal and General 315k */
+     when opp_id = '0061R000013fGLrQAM' then to_date('2022-12-15') /* End date adjustment for historical accuracy. Legal and General 315k */
      when opp_id = '0061R0000135gO1QAI' then to_date('2023-01-15') /* End date adjustment because of open negotiations. Accerta 89.4k */
-     when opp_id = '0061R000013fGLrQAM' then to_date('2022-12-15') /* End date adjustment because of open negotiations. Legal and General 315k */
      when opp_id = '0061R000013gx5GQAQ' then to_date('2023-01-15') /* End date adjustment because of open negotiations. Chanel 5k */
      when opp_id = '0061R00000uLcG2QAK' then to_date('2023-01-15') /* End date adjustment because of open negotiations. Morgan Stanley 148.5k */ 
      ELSE end_dte_raw end as end_dte,
