806410769cd76f94823f75594769c8d251a564ae
Erik Jones <erik.jones@hyperscience.com>
2023-01-25 21:19:50 +0000

    updated fct arr opp renewals model and added fct renewals model

--- models/transformed_opps_for_open_negotiations.sql
+++ models/transformed_opps_for_open_negotiations.sql
@@ -11,10 +11,11 @@ with opp_ids as (
 select distinct opp_id
 from {{ref('arr_opp_history')}}
 where opp_id in (
-'0061R0000137by9QAA', /* End date adjustment because of open negotiations. AIG 555k */
-'0061R0000137kdCQAQ', /* End date adjustment because of open negotiations. Unum 625k */
 '0061R0000135gO1QAI', /* End date adjustment because of open negotiations. Accerta 89.4k */
- '0061R000014wIeUQAU' /* End date adjustment because of open negotiations. SSA W2 950k */
+'0061R00000uLcG2QAK', /* End date adjustment because of open negotiations. Morgan Stanley 148.5k */ 
+'0061R0000137hXuQAI', /* End date adjustment because of open negotiations. FIS Global 130k */ 
+'0061R000013f0rkQAA', /* End date adjustment because of open negotiations. State of CO 214k */
+'0061R000014uXZrQAM'  /* End date adjustment because of open negotiations. MPower 99k */
 )
 )
 
