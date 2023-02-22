0e669dcf5f467449d618569efebb63c600d7742e
erik.jones@hyperscience.com <erik.jones@hyperscience.com>
2023-02-22 23:55:12 +0000

    updated all customer usage models

---
+++ models/customer_usage/data_qc_saas_usage.sql
@@ -0,0 +1,31 @@
+{{ config
+(
+    materialized='table',
+    database = 'PROD',
+    schema = 'CUSTOMER_USAGE'
+)
+}} 
+
+with prod_env as (
+select distinct
+domain,
+CASE WHEN domain ilike '%prod.%' then 1 else 0 end as prod_env_flag,
+split_part(domain,'.',0) as customer_int
+from "HEAP_MAIN_PRODUCTION"."HEAP"."PAGEVIEWS"
+where prod_env_flag = 1
+),
+
+saas_usage as (
+select distinct
+customer_int,
+customer
+from {{ref('saas_usage')}}
+),
+
+add_to_saas_usage as (
+select distinct customer_int 
+from prod_env
+where customer_int not in (select customer_int from saas_usage)
+)
+
+select * from add_to_saas_usage
