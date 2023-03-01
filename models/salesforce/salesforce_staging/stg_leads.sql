26b5e9bcb2be7cdf4679445397fc9d1b40f7d73a
christopher.mee@hyperscience.com <christopher.mee@hyperscience.com>
2023-03-01 12:17:14 +0000

    added disposition fields

--- models/salesforce/salesforce_staging/stg_leads.sql
+++ models/salesforce/salesforce_staging/stg_leads.sql
@@ -87,6 +87,7 @@ date(ifnull(date_stage_disqualifed_c, dq_date_c)) as dq_date,
     
 date(date_stage_customer_c) as customer_date,
 date(date_stage_former_customer_c) as former_customer_date,
-date(converted_date) as converted_date
+date(converted_date) as converted_date,
+disposition_c as disposition
  
 from {{ source('salesforce', 'lead')}}
