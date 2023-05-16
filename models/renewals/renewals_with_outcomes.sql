d114049610192b8d71df3ffcdd4827202cb3f8f9
erik.jones@hyperscience.com <erik.jones@hyperscience.com>
2023-05-16 20:22:59 +0000

    updated renewals code

--- models/renewals/renewals_with_outcomes.sql
+++ models/renewals/renewals_with_outcomes.sql
@@ -24,8 +24,10 @@ r.opp_name as renewal_opp_name,
 r.prior_opp_id,
 fao2.opp_arr as actual_renewal_amount,
 (round(actual_renewal_amount) - round(potential_churn_amount)) as renewal_diff,
+faa.is_active_acct,
 CASE WHEN renewal_diff = 0 then 'flat'
      WHEN renewal_diff < 0 then 'arr decrease'
+     WHEN renewal_diff IS NULL and faa.is_active_acct = true then 'arr decrease'
      WHEN renewal_diff > 0 then 'arr increase'
      ELSE 'logo churn' end as renewal_type,
 CASE WHEN renewal_type = 'arr decrease' then 1 else 0 end as renewal_with_arr_churn_flag,
@@ -37,6 +39,7 @@ from {{ref('opps_up_for_renewal')}} as ro
 left join {{ref('renewed_opps')}} as r on (ro.opp_id = r.prior_opp_id)
 left join {{ref('arr_opp_history')}} as fao1 on (ro.opp_id = fao1.opp_id)
 left join {{ref('arr_opp_history')}} as fao2 on (r.opp_id = fao2.opp_id )
+left join {{ref('fct_arr_account')}} as faa on (ro.account_id = faa.account_id and to_date(ro.date_month) = to_date(faa.date_month))
 where date_trunc(month,to_date(ro.end_dte)) <= date_trunc(month,to_date(current_date()))
 and ro.opp_id not in (select opp_id from {{ref('transformed_opps_for_open_negotiations')}})
 order by ro.date_month asc
