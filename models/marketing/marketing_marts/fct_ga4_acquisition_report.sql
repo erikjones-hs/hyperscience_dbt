1a8b66200882c4f7fbd6d2074ab38998265fb161
Christopher Mee <christopher.mee@hyperscience.com>
2023-08-02 08:50:06 +0000

    updated google analytics 4 data

--- models/marketing/marketing_marts/fct_ga4_acquisition_report.sql
+++ models/marketing/marketing_marts/fct_ga4_acquisition_report.sql
@@ -1,12 +1,7 @@
 select 
 
 date,
-session_campaign_name as utm_campaign,
-landing_page,
 session_default_channel_grouping as channel_grouping,
-session_source_medium as source_medium,
-split_part(session_source_medium, ' / ', 1) as utm_source,
-split_part(session_source_medium, ' / ', 2) as utm_medium,
 conversions,
 new_users,
 sessions,
@@ -15,5 +10,5 @@ event_count as events,
 engaged_sessions,
 screen_page_views
 
-from {{ source('google_analytics_4', 'acquisition_report') }}
+from {{ source('google_analytics_4', 'acquisition_report_version_2') }}
 order by date desc
