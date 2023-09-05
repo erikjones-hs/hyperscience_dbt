f7cc09aee4854300c4a024d0af99e3f7fc111ec8
erik.jones@hyperscience.com <erik.jones@hyperscience.com>
2023-09-05 04:47:03 +0000

    updated arr

--- models/transformed_opps_for_open_negotiations.sql
+++ models/transformed_opps_for_open_negotiations.sql
@@ -11,17 +11,12 @@ with opp_ids as (
 select distinct opp_id
 from {{ref('arr_opp_history')}}
 where opp_id in (
-'0061R000016kwHtQAI', /* End date adjustment because of open negotiations. Miss. Health 689k */ 
-'0061R000014wNrpQAE', /* End date adjustment because of open negotiations. QAI 35k */ 
 '0061R000014vUKMQA2', /* End date adjustment because of open negotiations. USAF 115k */ 
 '0061R000014wI4lQAE', /* End date adjustment because of open negotiations. FATCO 75k */ 
-'0061R000016myLZQAY', /* End date adjustment because of open negotiations. DOJ 41.5k */ 
 '0061R000014yeOrQAI', /* End date adjustment because of open negotiations. Mathematica 100k */
-'0061R000014wI4sQAE', /* End date adjustment because of open negotiations. Canada LIfe 71k */
 '0061R00000yGqH3QAK', /* End date adjustment because of open negotiations. SSA 2.3M */
 '006Dm000002cdEUIAY', /* End date adjustment because of open negotiations. VA VICCS 1.5M */
 '0061R000016nZwpQAE', /* End date adjustment because of open negotiations. VetsEZ 50k */
-'0061R000014wI34QAE', /* End date adjustment because of open negotiations. CompIQ 173k */
 '0061R00001A5k8bQAB', /* End date adjustment because of open negotiations. SSA 1.45M */
 '0061R000014wNrtQAE'  /* End date adjustment because of open negotiations. SSA 1.93M */ 
 )
