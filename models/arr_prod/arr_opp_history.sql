a23b9400569a2f78651ea8da6cbeab64d13f75be
erik.jones@hyperscience.com <erik.jones@hyperscience.com>
2023-11-22 22:34:50 +0000

    updated arr and customer usage

--- models/arr_prod/arr_opp_history.sql
+++ models/arr_prod/arr_opp_history.sql
@@ -178,6 +178,8 @@ UNION
 select * from {{ref('arr_opp_history_84')}}
 UNION
 select * from {{ref('arr_opp_history_85')}}
+UNION
+select * from {{ref('arr_opp_history_86')}}
 order by account_id, start_dte_raw asc
 )
 
