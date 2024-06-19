7b95dd1f478bc06ddfd4eae29069310ac91cde36
erik.jones@hyperscience.com <erik.jones@hyperscience.com>
2024-06-19 03:06:42 +0000

    updated arr

--- models/arr_prod/arr_opp_history_transformed.sql
+++ models/arr_prod/arr_opp_history_transformed.sql
@@ -157,6 +157,7 @@ CASE WHEN opp_id = '0063600000X36zWAAR' then to_date('2020-07-01')
    --  when opp_id = '0061R00001A5eXkQAJ' then to_date('2024-07-15') /* ADP */
      when opp_id = '0061R000010O3QoQAK' then to_date('2024-07-15') /* CDCR */
      when opp_id = '006Pm000008xmdVIAQ' then to_date('2024-07-15') /* Transflo */  
+     when opp_id = '0061R000016nPUDQA2' then to_date('2024-06-15') /* End date adjustment because of early churn. FTI 130k */
      ELSE end_dte_raw end as end_dte,
 end_dte_raw,
 CASE WHEN opp_id = '0061R00000uINyXQAW' then to_date('2020-08-01')
