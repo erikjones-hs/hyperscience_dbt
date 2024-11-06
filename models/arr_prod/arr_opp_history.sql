7c62ac55717363f77042bd580794362094cfd940
erik.jones@hyperscience.com <erik.jones@hyperscience.com>
2024-11-06 21:09:45 +0000

    updated arr

--- models/arr_prod/arr_opp_history.sql
+++ models/arr_prod/arr_opp_history.sql
@@ -250,6 +250,8 @@ UNION
 select * from {{ref('arr_opp_history_120')}}
 UNION
 select * from {{ref('arr_opp_history_121')}}
+UNION
+select * from {{ref('arr_opp_history_122')}}
 order by account_id, start_dte_raw asc
 )
 
