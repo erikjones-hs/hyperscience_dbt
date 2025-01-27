70a4bc9573695e2a6849c87380c31e8fd9686e41
Ryan Severns <ryan@floodlightgrowth.com>
2025-01-27 22:31:42 +0000

    adding update for leads that did not have email addresses

--- models/marketing/activation_marketing/crm_created_date.sql
+++ models/marketing/activation_marketing/crm_created_date.sql
@@ -12,8 +12,9 @@ select
     ,l.created_date as sfdc_lead_created_date
     ,co.created_date as sfdc_contact_created_date
 from {{ ref('int_hubspot_contacts')}} c
-left join {{ ref('int_sf_lead')}} l
-    on c.email = l.email
 left join {{ ref('int_sf_contact')}} co
     on c.email = co.email
+left join {{ ref('int_sf_lead')}} l
+    on c.email = l.email
+        or co.id = l.converted_contact_id
 order by c.createdate desc
